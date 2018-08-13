4dace0e47a3cdfc3ba521bb82b76b1d1a87a050d
Stevie <steviepinero@gmail.com>
2018-08-13 17:04:41 -0400

    Update Main.java

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -4,29 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
     	
-	double val1 = 100.0d;
-	double val2 = 50.0d;
-	double result;
-	char opCode = 'a';
-
-	if (opCode == 'a')
-	    result = val1 + val2;
-	else if (opCode == 's')
-	    result = val1 - val2;
-	else if (opCode == 'd') {
-		result = val2 != 0.0d ? val1 / val2 : 0.0d;
-//		if ( val2 != 0.0d)
-//			result = val1 / val2;
-//		else
-//			result = 0.0d;
-	}
-	else if (opCode == 'm')
-	    result = val1 * val2;
-	else
-		System.out.println("Error wrong op code");
-	    result = 0.0d;
+ MathEquation equations = new MathEquation[4];
+        equations[0] = new MathEquation();
 
 
-	System.out.println(result);
-    }
+        equations[0] = new MathEquation('d',100.0d, 50.0d);
+        equations[1] = new MathEquation('a',25.0d, 92.0d);
+        equations[2] = new MathEquation('s',225.0d, 17.0d);
+        equations[3] = new MathEquation('m',11.0d, 3.0d);
+
+        for (MathEquation equation : equations) {
+            equation.execute();
+            System.out.println(equation.getResult);
+        }
 }
